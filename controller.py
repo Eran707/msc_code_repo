02f7e7e480f571d23853130c17a3d3800eb6611f
Eran707 <eshorer96@gmail.com>
2022-06-22 14:53:59 +0200

    Clear up of unused files in the repo

--- controller.py
+++ controller.py
@@ -1,42 +1,78 @@
 """
-Main script to run simulation
+Main script to control the neural simulation.
+
+# STEP 1: INITIALIZE SIMULATION AND DEFINE MORPHOLOGY:
+
+    - Define the file name (simulation name) where the results of the simulation will be saved to
+    - The file will be saved to the root directory
+    - Either add the default multicompartment which adds a set number of defined compartments
+        This function allows an option to add a somatic compartment which creates a compartment that is double the size
+    - Or, add individual compartments
+
+# STEP 2: SIMULATION SETTINGS:
+
+    - set_electrodiffusion_properties: turn electrodiffusion on/off and define diffusion constants
+    - set_external_ion_properties: option to change external ion concentrations away from default
+    - set_atpase_static: use the initialization Na concentration to fix the Na-K-ATPase pump rate
+    - set_sa_static: ensure that the surface area of each compartment does not change
+    - set_hh: add hodgkin huxley channels to a compartment
+    - set_timing: define the time step, total time, and intervals (number of times to save to file)
+
+# STEP 3: ADDITIONAL ION FLUXES SETTINGS:
+
+    - set_zflux: change impermeant anion valence
+    - set_xflux: change impermeant anion quantity
+    - add_current: add external current source
+    - add_synapse: add a synapse
+
+# STEP 4: RUN SIMULATION
+    - write_settings_to_file: option to make a text file in the root directory which contains the simulation settings
+    - run_simulation: runs the simulation
+
+Creator: EF Shorer
 """
 
-import simulator3
+###################################################################
+# 1) DEFINE SIMULATOR CLASS AND DEFINE MORPHOLOGY
+###################################################################
+import simulator
+file_name = "SS_gk80_HH"
 
-file_name = "SS_C4_065_HH"
-
-# 1) DEFINE SIMULATOR CLASS AND ADD COMPARTMENTS
-sim = simulator3.Simulator(file_name)
-
+sim = simulator.Simulator(file_name)
 sim.add_default_multicompartment(number_of_comps=9, rad=0.5e-5, len=20e-5, soma=True)
+#sim.add_compartment()
 
+##################################################################
 # 2) SET SIMULATION SETTINGS
-
+##################################################################
 sim.set_electrodiffusion_properties(ED_on=True, diff_constant_dict={"na": (1.33 / 2) * 1e-7, "k": (1.96 / 2) * 1e-7,
                                                                     "cl": (2.03 / 2) * 1e-7, "x": 0})
 sim.set_external_ion_properties()
 sim.set_atpase_static(static_atpase=True)
 sim.set_sa_static(static_sa=True)
-
-total_t = 450
-time_step = 1e-6
-sim.set_zflux(comps=["Comp4"], start_t=80e-4, end_t=120e-4, z_end=-0.65, adjust_x=True)
-#sim.set_xflux(comps=['Comp8'], start_t=0.5, end_t=0.7, flux_rate=20e-16 / 60, z=-0.85)
-sim.set_timing(total_t=total_t, time_step=time_step, intervals=1000)
 # sim.set_hh_on(comp="Comp0(Soma)", t_on=0)
+
+total_t = 30
+time_step = 1e-6
+sim.set_timing(total_t=total_t, time_step=time_step, intervals=1000)
+
+##################################################################
+# 3) ADDITIONAL ION FLUX SETTINGS
+##################################################################
+
+#sim.set_zflux(comps=["Comp4"], start_t=80e-4, end_t=120e-4, z_end=-0.65, adjust_x=True)
+#sim.set_xflux(comps=['Comp8'], start_t=0.5, end_t=0.7, flux_rate=20e-16 / 60, z=-0.85)
+#sim.add_synapse("Comp1", "Excitatory", start_t=0.5, duration=5 * 1e-3, max_neurotransmitter=1e-3, synapse_conductance=1e-9)
+#sim.add_current("Comp9","Excitatory",start_t=10e-3,duration=1e-3, current_A=0.1e-9, total_t=total_t,frequency=1)
+
+
+
+##################################################################
+# 4) RUN SIMULATION
+##################################################################
+
 sim.write_settings_to_file()
 sim.run_simulation()
 print("fin")
 
-# sim.add_synapse("Comp1", "Excitatory", start_t=0.5, duration=5 * 1e-3, max_neurotransmitter=1e-3, synapse_conductance=1e-9)
-# sim.add_current("Comp9","Excitatory",start_t=10e-3,duration=1e-3, current_A=0.1e-9,
-# total_t=total_t,frequency=1)
 
-# sim.set_xflux(comps=["Comp8"], start_t=120, end_t=180, z=-0.85, flux_rate=300*1e-3/60)
-
-
-# sim.set_zflux()
-# sim.set_xoflux()
-
-# 4) RUN SIMULATION
--- controller_from_hdf.py
+++ controller_from_hdf.py
@@ -10,12 +10,12 @@ Revised: April 2022
 
 import simulator_from_hdf
 
-old_file_name = "SS_085_HH_v2"
-new_file_name = "SS_gk200_HH"
+old_file_name = "SS_gk80_HH"
+new_file_name = "Exp6_gk80_65"
 amend_type = 'LastValues'
 
 sim = simulator_from_hdf.SimulatorFromHDF(old_file_name, new_file_name, amend_type, already_extended=False)
-sim.set_timing(total_t=200e-3, dt=1e-6, intervals=1000)
+sim.set_timing(total_t=200e-3, dt=1e-6, intervals=10000)
 # sim.set_timing(extend_t=100)
 sim.set_electrodiffusion_properties(ED_on=True)
 sim.set_external_ion_properties()
@@ -23,15 +23,12 @@ sim.set_sa()
 sim.set_static_sa()
 sim.set_na_i_start()
 sim.set_atpase_static(static_atpase=True)
-sim.set_gkcc2(comp_name="Comp4", gkcc2=200e-3)
+sim.set_gkcc2(comp_name="Comp4", gkcc2=8e-3)
 sim.set_hh_on("Comp0(Soma)", t_on=0)
-# sim.add_synapse("Comp8", "Excitatory", start_t=50e-3, duration=5 * 1e-3,
-# max_neurotransmitter=3e-3, synapse_conductance=5.8e-9)
-# sim.add_synapse("Comp4", "Inhibitory", start_t=(50.1)*1e-3, duration=5 * 1e-3,
-# max_neurotransmitter=3e-3, synapse_conductance=12e-9)
+sim.add_synapse("Comp8", "Excitatory", start_t=50e-3, duration=5 * 1e-3,
+                max_neurotransmitter=3e-3, synapse_conductance=6.5e-9)
+sim.add_synapse("Comp4", "Inhibitory", start_t=(50.1)*1e-3, duration=5 * 1e-3,
+                max_neurotransmitter=3e-3, synapse_conductance=10e-9)
 
-
-# sim.comp_arr[7].adjust_x_bool = True
-# sim.add_current(comp_name='Comp9', current_type="Excitatory", start_t=1e-3, duration=1e-3, current_A=0.1e-9)
 sim.write_settings_to_file(old_file_name)
 sim.run_simulation()
